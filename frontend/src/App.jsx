9aa6512d0ad906bccd7bb7f103b83bdd06f115fd
vasanthakumar-a <vasanthakumara117@gmail.com>
2025-03-31 23:50:32 +0530

    Added subdomain with admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,24 @@ import store from "./redux/store";
 
 const queryClient = new QueryClient();
 
+const getSubdomain = () => {
+  const host = window.location.hostname; // e.g., "admin.localhost"
+  const parts = host.split(".");
+  if (parts.length > 1) {
+    return parts[0]; // Extract "admin" from "admin.localhost"
+  }
+  return "home"; // Default to home
+};
+
+
 export default function App() {
+  const subdomain = getSubdomain();
+
   return (
     <Provider store={store}>
       <PrimeReactProvider value={{ unstyled: false, pt: Tailwind, ripple: true }}>
         <QueryClientProvider client={queryClient}>
-          <AppRoutes />
+        {subdomain === "admin" ? <>ADMIN</> : <AppRoutes />}
         </QueryClientProvider>
       </PrimeReactProvider>
     </Provider>
